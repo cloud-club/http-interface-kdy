9b286a75a6f0f74f2fb1148d83ac4e79420814f7
Kimdoyoung <52124111+dev-kimdoyoung@users.noreply.github.com>
2023-10-14 11:07:16 +0900

    Update common.go
    asd

--- types/common.go
+++ types/common.go
@@ -8,6 +8,6 @@ import (
 func GetCommonHeader(request *http.Request) {
 	request.Header.Set("Accept", "application/vnd.github+json")
 	request.Header.Set("cache-control", "max-age=0")
-	request.Header.Set("Authorization", fmt.Sprintf("Bearer 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"))
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer "))
 	request.Header.Set("X-GitHub-Api-Version", "2022-11-28")
 }
